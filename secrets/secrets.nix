42fac715fa69184d994272ba440762fb02023f78
kossLAN <kosslan@kosslan.dev>
2024-12-19 22:13:19 -0500

    secrets: rekeying

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,7 +1,7 @@
 let
   backup = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMw1hLInzGOHV+b0TNmQbl0HHR4cabnZjPdbXcqkSNk";
   cerebrite = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPQc5Or080Vx65xnRe8VnCOKUobhOqdCFvWC06zanvWt";
-  galahad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOZglP92ZxsqI4wRzGR/A3kEQIQPB94pwysihwo9bT+E";
+  galahad = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDMw1hLInzGOHV+b0TNmQbl0HHR4cabnZjPdbXcqkSNk";
   dahlia = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBoPlufvtH9l19yYmw56vLCrZH84imK3pV5UHm98j8wn";
 
   systems = [backup cerebrite galahad dahlia];
